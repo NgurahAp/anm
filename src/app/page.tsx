f9acc6fa0c8808da6d45098d919f750cedc4c214
Arya <ngurahpratama2002@gmail.com>
2024-12-19 15:26:05 +0700

    Done products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Navbar from "./components/landing/ui/navbar";
-import Footer from "./components/landing/ui/footer";
+
 import HeroSection from "./components/landing/section/HeroSection";
 import AboutUs from "./components/landing/section/AboutUsSection";
 import WhyUs from "./components/landing/section/WhyUsSection";
 import Produts from "./components/landing/section/Products";
 import HowToOrder from "./components/landing/section/HowToOrder";
-import Testimonial from "./components/landing/section/testimonial";
 import ContactUs from "./components/landing/section/ContactUs";
+import Navbar from "./components/landing/ui/Navbar";
+import Testimonial from "./components/landing/section/Testimonial";
+import Footer from "./components/landing/ui/Footer";
 
 export default function Home() {
   return (
