4a333075119e035d2ea02692d5268268ac40db99
Arya <ngurahpratama2002@gmail.com>
2024-12-20 11:11:29 +0700

    Done spesifikasi section

--- src/app/products/[productName]/page.tsx
+++ src/app/products/[productName]/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
+import { ChevronDown } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
 import { use } from "react";
 
 interface Params {
@@ -16,9 +15,6 @@ interface PageProps {
 export default function ProductDetail({ params }: PageProps) {
   const resolvedParams = use(params as Promise<{ productName: string }>);
   const { productName } = resolvedParams;
-
-  const [isModePadatOpen, setIsModePadatOpen] = useState<boolean>(false);
-  const [isModeCairOpen, setIsModeCairOpen] = useState<boolean>(false);
 
   const decodedProductName = decodeURIComponent(productName);
 
@@ -43,6 +39,16 @@ export default function ProductDetail({ params }: PageProps) {
     "MD-300S dapat mengukur densitas cairan terkompensasi dengan pengaturan suhu cairan terkompensasi dan laju suhu kompensasi.",
   ];
 
+  const spesifikasi: string[] = [
+    "Model : MD-300S",
+    "Resolusi : 0.001g/cm",
+    "Kapasitas : 0.01~300g",
+    "Fitur : Padat, Cair",
+    "Berat : 1.6kg",
+    "Dimensi : (D) 218 x (W) 190 x (H) 170mm",
+    "Asesoris : Pinset, Termometer, Berat Kalibrasi 200g, Sudut Baja, Adaptor AC, Mudah terhubung ke PC dengan perlengkapan standar RS232C, Kaca depan kedap udara."
+  ]
+
   if (!validProducts.includes(decodedProductName)) {
     return (
       <div className="min-h-screen px-10 py-10">
@@ -56,107 +62,87 @@ export default function ProductDetail({ params }: PageProps) {
     );
   }
 
-  const handleModePadatToggle = () => {
-    setIsModePadatOpen((prev) => !prev);
-  };
-
-  const handleModeCairToggle = () => {
-    setIsModeCairOpen((prev) => !prev);
-  };
-
   return (
-    <div className="min-h-screen px-10 pt-32">
-      <div className="flex">
-        <div className="w-1/2 pl-20">
-          <Image
-            src="/landing/md-300s.jpeg"
-            alt="Densimeter"
-            width={400}
-            height={400}
-            priority
-          />
-        </div>
-        <div className="w-1/2 pt-10 px-5">
-          <h2 className="font-urbanist font-extrabold text-5xl pr-10 text-gray-800">
-            Alfa Mirage Densimeter MD-300S
-          </h2>
-          <h3 className="pt-8 pb-2 font-urbanist text-xl font-extrabold">
-            Deskripsi
-          </h3>
-          <p className="text-gray-500">
-            Model populer dengan resolusi 0,001 g / cm yang ditingkatkan dari
-            sebelumnya model MD-200S. Bodi kompak dan pengukuran kepadatan yang
-            akurat untuk benda padat dan sampel cair.
-          </p>
-          <h3 className="pt-8 pb-2 font-urbanist text-xl font-extrabold">
-            Fungsi
-          </h3>
+    <section>
+      <div className="min-h-screen px-10 pt-32">
+        <div className="flex">
+          <div className="w-1/2 pl-20 pr-16">
+            <Image
+              src="/landing/md-300s.jpeg"
+              alt="Densimeter"
+              width={400}
+              height={400}
+              priority
+            />
+            <h3 className="pt-8 pb-2  font-urbanist text-xl font-extrabold">
+              Spesifikasi
+            </h3>
+            {spesifikasi.map((feature, index) => (
+              <div key={index} className="flex items-start gap-3 pt-2 list-none">
+                <span className="text-green-700">•</span>
+                <li className="text-gray-500 pl-1">{feature}</li>
+              </div>
+            ))}
+          </div>
+          <div className="w-1/2 pt-10 px-5">
+            <h2 className="font-urbanist font-extrabold text-5xl pr-10 text-gray-800">
+              Alfa Mirage Densimeter MD-300S
+            </h2>
+            <h3 className="pt-8 pb-2 font-urbanist text-xl font-extrabold">
+              Deskripsi
+            </h3>
+            <p className="text-gray-500">
+              Model populer dengan resolusi 0,001 g / cm yang ditingkatkan dari
+              sebelumnya model MD-200S. Bodi kompak dan pengukuran kepadatan
+              yang akurat untuk benda padat dan sampel cair.
+            </p>
+            <h3 className="pt-8 pb-2 font-urbanist text-xl font-extrabold">
+              Fungsi
+            </h3>
 
-          {/* Mode Padat Dropdown */}
-          <div className="mb-4">
-            <button
-              onClick={handleModePadatToggle}
-              type="button"
-              className="w-full flex justify-between items-center py-2 px-4 bg-gray-100 hover:bg-gray-100 rounded-t-lg transition-all duration-300"
-            >
-              <span className="font-urbanist font-medium">
-                Fungsi Mode Padat
-              </span>
-              {isModePadatOpen ? (
-                <ChevronUp className="w-5 h-5 text-gray-600" />
-              ) : (
+            {/* Mode Padat Section */}
+            <div className="mb-4">
+              <div className="w-full py-2 px-4 bg-gray-100 rounded-t-lg flex justify-between">
+                <span className="font-urbanist font-medium">
+                  Fungsi Mode Padat
+                </span>
                 <ChevronDown className="w-5 h-5 text-gray-600" />
-              )}
-            </button>
-            {isModePadatOpen && (
-              <div className=" pl-4 py-2 bg-gray-50 rounded-b-lg">
+              </div>
+              <div className="pl-4 py-2 bg-gray-50 rounded-b-lg">
                 <ul className="space-y-1">
                   {modePadatFeatures.map((feature, index) => (
                     <div key={index} className="flex items-center gap-3">
-                      {/* Bullet point */}
                       <span className="text-green-700">•</span>
-                      {/* Text */}
-                      <li className="text-gray-600 text-sm  pl-1">{feature}</li>
+                      <li className="text-gray-600 text-sm pl-1">{feature}</li>
                     </div>
                   ))}
                 </ul>
               </div>
-            )}
-          </div>
+            </div>
 
-          {/* Mode Cair Dropdown */}
-          <div className="mb-4">
-            <button
-              onClick={handleModeCairToggle}
-              type="button"
-              className="w-full flex justify-between items-center py-2 px-4 bg-gray-100 hover:bg-gray-100 rounded-t-lg transition-all duration-300"
-            >
-              <span className="font-urbanist font-medium">
-                Fungsi Mode Cair
-              </span>
-              {isModeCairOpen ? (
-                <ChevronUp className="w-5 h-5 text-gray-600" />
-              ) : (
+            {/* Mode Cair Section */}
+            <div className="mb-4">
+              <div className="w-full py-2 px-4 bg-gray-100 rounded-t-lg flex justify-between">
+                <span className="font-urbanist font-medium">
+                  Fungsi Mode Cair
+                </span>
                 <ChevronDown className="w-5 h-5 text-gray-600" />
-              )}
-            </button>
-            {isModeCairOpen && (
-              <div className=" pl-4 py-2 bg-gray-50 rounded-b-lg">
-                <ul className="space-y-1  ">
+              </div>
+              <div className="pl-4 py-2 bg-gray-50 rounded-b-lg">
+                <ul className="space-y-1">
                   {modeCairFeatures.map((feature, index) => (
                     <div key={index} className="flex items-center gap-3">
-                      {/* Bullet point */}
                       <span className="text-green-700">•</span>
-                      {/* Text */}
-                      <li className="text-gray-600 text-sm  pl-1">{feature}</li>
+                      <li className="text-gray-600 text-sm pl-1">{feature}</li>
                     </div>
                   ))}
                 </ul>
               </div>
-            )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div></div>
+    </section>
   );
 }
